b205f5052073e328d5c5ca45338364762e90eb74
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-04-30 11:52:25 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Logout from "./components/Logout";
 import Signup from "./components/Signup";
 import Error from "./components/Error";
 import Room from "./components/Room";
+import Administration from "./components/Administration";
 
 
 function App() {
@@ -34,6 +35,7 @@ function App() {
           <Route path="/logout" element={<Logout />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/hostel/:hostelId/room/:roomId" element={<Room />} />
+          <Route path="/administration" element={<Administration />} />
 
 
           <Route path="*" element={<Error />} />
