cf7072725b0cc5dbaa5d449cfea60bd428c7c990
Kawchar Husain <kawchar85@yahoo.com>
2022-07-11 20:54:27 +0600

    styling error page

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import CardGroup from 'react-bootstrap/CardGroup'
 
 export default function Error() {
+
     return (
-        <div><h1>401 Error | Page not found</h1></div>
+        <>
+            <br /><br />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <CardGroup>
+                    <Card border="primary" style={{ width: '80%' }}>
+                        <Card.Img variant="top" src="images/404.png" width="100px" height="600px" />
+                    </Card>
+                    <Card bg="light" border="primary" style={{ width: '18rem' }}>
+                        <Card.Header><h2>401 Error | Page not found</h2></Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                Sorry, but nothing matched your search terms. Please try again with some different keywords.
+                            </Card.Text>
+                            <a href="home"><Button variant="primary">Go To Home</Button></a>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </div>
+            
+
+        </>
     )
 }
