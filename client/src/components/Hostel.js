89be822d68199064115cec31b6bc83267b86efdd
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-08-10 16:56:35 +0600

    Update Hostel.js

--- client/src/components/Hostel.js
+++ client/src/components/Hostel.js
@@ -10,46 +10,98 @@ import RoomDetails from './RoomDetails';
 export default function Hostel() {
 
   const {id} = useParams();
-  const [modalShow, setModalShow] = React.useState(false);
-  const [hostelData, setHostelData] = useState(null);
+  const [modalShow, setModalShow] = React.useState([]);
+  const [hostelData, setHostelData] = useState({
+    Hostel_ID: 0,
+    Type: "Loading...",
+    Name: "Hostel Name Loading...",
+    Address: "Loading...",
+    Contact: "Loading...",
+    Total_Seats: 0,
+    Occupied_Seats: 0,
+});
+
+const [rooms, setRooms] = useState([]);
+const [roomID, setRoomID] = useState(0);
 
   const getData = async () => {
 
-     await Axios.get("http://localhost:3001/getData/hostel", { params: { hostel_id: id } }).then((response) => {
+    await Axios.get("http://localhost:3001/getData/hostel", { params: { hostel_id: id } }).then((response) => {
         if(response.data === "error"){
-            alert("Something Error!!");
+            alert("Invalid Hostel ID!!!");
         } else {
-            let data = JSON.stringify(response.data[0]);
-            console.log("res data");
-            console.log(data);
-            setHostelData(data);
-            console.log("hostel data");
-            console.log(hostelData);
+              let data = response.data[0];
+              console.log("res data");
+              console.log(data);
+              setHostelData({...hostelData,Hostel_ID: data.Hostel_ID, Type:data.Type, Name: data.Name, Address: data.Address, Contact: data.Contact, Total_Seats: data.Total_Seats, Occupied_Seats: data.Occupied_Seats});
         }
     }).catch((e) => alert(e));
+
   };
+  
+    const getRooms = async () => {
+    await Axios.get("http://localhost:3001/getData/rooms", { params: { hostel_id: id } }).then((response) => {
+          if(response.data === "error"){
+              alert("Something Error");
+          } else {
+              setRooms(response.data);
+              const arr = Array(response.data.length).fill(false);
+              console.log("length ----> ");
+              console.log(arr);
+              setModalShow(arr);
+          }
+        }).catch((e) => alert(e));
+    };
   
   useEffect(() => {
     console.log("data loading");
     getData();
-
-  }, [id]);
+    getRooms();
+  }, []);
 
   useEffect(() => {
     console.log("Loaded data");
     console.log(hostelData);
-  }, [hostelData]);
+    console.log(rooms)
+  }, [hostelData,rooms]);
 
   const handleClick = e => {
     e.preventDefault();
-    console.log(e.target.id);
-    setModalShow(true);
-    
+    console.log("clicked");
+    const id = e.target.id;
+    console.log(rooms[id].Room_ID);
+    setRoomID(rooms[id].Room_ID);
+    const arr = modalShow;
+    arr[id] = true;
+    setModalShow(arr);
+    console.log(modalShow);
   };
+
+  const handleClose = (id) => {
+    console.log("closing..."+id);
+    const arr = modalShow;
+    arr[id] = false;
+    setModalShow(arr);
+    console.log(arr);
+
+  };
+
+  const getShow = (id) =>{
+    console.log("get..."+id+" = "+modalShow[id]);
+    return modalShow[id];
+  }
+  
+  const getModal = ()=>{
+    return (<RoomDetails 
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+              id={roomID}
+              hostel_id = {id}
+            />)
+  }
+
   return (
     <>
-    <div>Hostel {id} </div>
-
     <div style={{
             textShadow: "#f9fafb 0px 1px 0px, #0d6efd 3px 3px 3px",
             textAlign: "center",
@@ -58,9 +110,9 @@ export default function Hostel() {
             letterSpacing: "4px",
             font: "normal 30px/2 Segoe Print,Verdana, Helvetica",
             position: "relative",
-        }} >
-          {JSON.parse(hostelData)}
+        }} >          
           
+          {hostelData.Name}
         </div>
 
         <div className="shadow p-4" style={{
@@ -89,12 +141,14 @@ export default function Hostel() {
                 letterSpacing: "4px",
                 font: "normal 20px/1 Comic Sans MS,Verdana, Helvetica",
             }}>
-              Total Seats : {JSON.parse(hostelData).Total_Seats} <br/>
-              Occupied Seats : {JSON.parse(hostelData).Occupied_Seats} <br/>
-              Type : {JSON.parse(hostelData).Type} <br/>
+              Total Seats : {hostelData.Total_Seats} <br/>
+              Occupied Seats : {hostelData.Occupied_Seats} <br/>
+              Type : {hostelData.Type} <br/>
               Provost : ???(different card?)<br/> 
-              Office : {JSON.parse(hostelData).Address} <br/>
-              Contact : {JSON.parse(hostelData).Contact}
+              Office : {hostelData.Address} <br/>
+              Contact : {hostelData.Contact}
+
+                
             </div>
         
         </div>
@@ -126,14 +180,22 @@ export default function Hostel() {
                 letterSpacing: "4px",
                 font: "normal 20px/1 Comic Sans MS,Verdana, Helvetica",
             }}>
-              <Button id={1} variant="link" onClick={e => handleClick(e)} >1</Button>, 2, 3, 4, 5, 6 <br/>
-              7, 8, 9, 10, 11, 12<br/>
 
-              <RoomDetails 
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                    id={12}
-              />
+              {rooms.map((val, idx) => {
+                  return <>
+                          <Button id={idx} variant="link" onClick={e => handleClick(e)} >{val.Room_ID}, </Button>
+                          <RoomDetails 
+                              show={getShow}
+                              onHide={handleClose}
+                              id={idx}
+                              room_id = {val.Room_ID}
+                              hostel_id = {id}
+                          />
+                        </>
+                }
+              )}
+
+              
               
             </div>
         
