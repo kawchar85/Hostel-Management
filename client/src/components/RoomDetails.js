1225c24c61f39913b6297e7f8dd7cf87fa58316b
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-08-10 16:57:17 +0600

    add modal

--- client/src/components/RoomDetails.js
+++ client/src/components/RoomDetails.js
@@ -1,42 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import SingleStudent from './SingleStudent';
 
 
 export default function RoomDetails(props) {
 
-    console.log(props);
+    const [student, setStudent] = useState([]);
+    
+    const getData = async () => {
+    
+        await Axios.get("http://localhost:3001/getData/student/room", { params: { hostel_id: props.hostel_id, room_id: props.room_id } }).then((response) => {
+            if(response.data === "error"){
+                alert("Something erroe!!");
+            } else {
+                setStudent(response.data);
+            }
+        }).catch((e) => alert(e));
+    
+    };
+
+    useEffect(() => {
+        console.log("loading room details");
+        
+        getData();
+
+    }, []);
+    useEffect(() => {
+        console.log("data in room details");
+        console.log(student);
+    }, [student]);
 return (
     <>
 
-<Modal
-        show={props.show}
-        onHide={props.onHide}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-    >
-        <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-                Room Details
-            </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <h4>Room #{props.id}</h4>
-            <p>
+        <Modal
+            show={props.show(props.id)}
+            onHide={props.onHide(props.id)}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Room Details
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h4>Room #{props.room_id}</h4>
+                <p>
 
-            Hotel id diye query kore student list ber korbo(useEffect). 
-            Info in each card:
-            Std name: .....
-            Reg: ....
-            Dept: ....
-            Phone ....
-            </p>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-    </Modal>
+                Hotel id diye query kore student list ber korbo(useEffect). 
+                Info in each card:
+                Std name: .....
+                Reg: ....
+                Dept: ....
+                Phone ....
+                </p>
+
+                {
+                        student.map((val) => {
+                            return <SingleStudent data={val} />
+                        })
+                }
+
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide(props.id)}>Close</Button>
+            </Modal.Footer>
+        </Modal>
 
     </>
 
