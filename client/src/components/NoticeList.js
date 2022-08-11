9529b923019ae05d18d03e4819d3344a61f6f121
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-08-11 18:27:59 +0600

    UI update

--- client/src/components/NoticeList.js
+++ client/src/components/NoticeList.js
@@ -4,15 +4,33 @@ import Axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import UpdateHostel from './UpdateHostel';
 import UpdateNotice from './UpdateNotice';
+import RemoveNotice from './RemoveNotice';
 
 export default function NoticeList() {
 
     const [notices, setNotices] = useState([]);
     const [edit, setEdit] = useState(false);
+    const [del, setDel] = useState(false);
+    const [delID, setDelID] = useState(0);
     const [data, setData] = useState(0);
+    const [dateSort, setDateSort] = useState({
+        up : true,
+        down : true,
+    });
+
+    const [sortBy, setSortBy] = useState({
+        name: "Priority",
+        type: "DESC",
+    });
+
+    // const getNotices = () => {
+    //     Axios.get("http://localhost:3001/getData/", { params: { table: "notice_board" } }).then((response) => {
+    //         setNotices(response.data);
+    //     });
+    // };
 
     const getNotices = () => {
-        Axios.get("http://localhost:3001/getData/", { params: { table: "notice_board" } }).then((response) => {
+        Axios.get("http://localhost:3001/getData/notice", { params: { name: sortBy.name, type: sortBy.type } }).then((response) => {
             setNotices(response.data);
         });
     };
@@ -21,12 +39,13 @@ export default function NoticeList() {
         getNotices();
     }, []);
     useEffect(() => {
-        console.log("loaded data");
-        console.log(data);
-    }, [data,edit]);
-    const getLess = (s)=>{
-        if(s.length<10) return s;
-        else return s.substring(0,7)+"...";
+        getNotices();
+        console.log(notices)
+    }, [sortBy]);
+    
+    const getLess = (s) => {
+        if (s.length < 10) return s;
+        else return s.substring(0, 7) + "...";
     }
 
     const handleClick = e => {
@@ -35,42 +54,75 @@ export default function NoticeList() {
         setEdit(true);
         setData(id);
     }
+    const handleDelete = e => {
+        e.preventDefault();
+        const id = e.target.id;
+        setDel(true);
+        setDelID(id);
+    }
+    
 
-    const showEditPage = ()=>{
-        if(edit) return (<UpdateHostel notice={data}/>);
-        else return(<></>);
+    const handleDateAsc = () =>{
+        console.log("date asc");
+        setSortBy({name: "Priority", type: "ASC"});
+        setDateSort({down: true, up: false});
+    }
+    const handleDateDesc = () =>{
+        console.log("date Desc");
+        setSortBy({name: "Priority", type: "DESC"});
+        setDateSort({down: false, up: true});
     }
 
-    if(!edit)
-    return (
-        <>
-            <div className="shadow p-4" style={{
-                width: "85%",
-                border: "3px solid lightGray",
-                marginLeft: "auto",
-                marginRight: "auto",
-            }} >
+    if (!edit && !del)
+        return (
+            <>
+                <div className="shadow p-4" style={{
+                    width: "85%",
+                    border: "3px solid lightGray",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                }} >
 
-                <Table striped bordered hover>
-                    <thead style={{
-                        color: "blue",
-                        letterSpacing: "2px",
-                        font: "normal 20px/1 Comic Sans MS,Verdana, Helvetica",
-                    }}>
-                        <tr>
-                            <th>Date Time</th>
-                            <th>Title</th>
-                            <th>Hostel ID</th>
-                            <th>Priority</th>
-                            <th>Description</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                    <Table striped bordered hover>
+                        <thead style={{
+                            color: "blue",
+                            letterSpacing: "2px",
+                            font: "normal 20px/1 Comic Sans MS,Verdana, Helvetica",
+                        }}>
+                            <tr>
+                                <th>
+                                    Date Time
+                                    {dateSort.up && (
+                                    <button onClick={handleDateAsc} style={{
+                                        border: "none",
+                                        padding: "0px",
+                                        background: "none",
+                                    }} >
+                                        &#9650;
+                                    </button>)}
 
-                        {notices.map((val, idx) => {
-                            return <React.Fragment key={idx}>
+                                    {dateSort.down && (
+                                    <button onClick={handleDateDesc} style={{
+                                        border: "none",
+                                        padding: "0px",
+                                        background: "none",
+                                    }} >
+                                        &#9660;
+                                    </button>)}
+                                    
+                                </th>
+                                <th>Title</th>
+                                <th>Hostel ID</th>
+                                <th>Priority</th>
+                                <th>Description</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {notices.map((val, idx) => {
+                                return <React.Fragment key={idx}>
                                     <tr>
                                         <td>{val.DateTime}</td>
                                         <td>{getLess(val.Title)}</td>
@@ -78,20 +130,22 @@ export default function NoticeList() {
                                         <td>{val.Priority}</td>
                                         <td>{getLess(val.Description)}</td>
                                         <td><Button id={idx} key={idx} variant="link" onClick={e => handleClick(e)} >Edit</Button></td>
-                                        <td>del</td>
+                                        <td><Button id={idx} key={idx} variant="link" onClick={e => handleDelete(e)} >Delete</Button></td>
                                     </tr>
-                    
-                                    </React.Fragment>
-                            }
-                        )}
-                        
-                        
-                    </tbody>
-                </Table>
 
-            </div>
-            <br /><br />
-        </>
-    )
-    else return (<UpdateNotice notice={notices[data]} />)
+                                </React.Fragment>
+                            }
+                            )}
+
+
+                        </tbody>
+                    </Table>
+
+                </div>
+                <br /><br />
+            </>
+        )
+    else if (edit) return (<UpdateNotice notice={notices[data]} />)
+    else if (del) return (<RemoveNotice notice={notices[delID]} />)
+
 }
